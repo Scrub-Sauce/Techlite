2dbaa23ca8ac6e8065e47dd2cf73634ea9d0cc20
Lucas Overbey <lucasaoverbey@gmail.com>
2022-04-21 17:07:09 -0500

    updated my last push

--- assets/php/orderListAdmin.php
+++ assets/php/orderListAdmin.php
@@ -1,5 +1,6 @@
 <?php include 'header.php';
       include_once ".env.php";
+      include 'adminHeader.php';
 ?>
     <div class = "container orders">
         <div class="alert alert-success" role="alert">
---
+++ assets/php/userListAdmin.php
@@ -0,0 +1,12 @@
+<?php include 'header.php'; 
+      include_once ".env.php";
+      include 'adminHeader.php';
+?>
+    <div class = "container orders">
+        <div class="alert alert-success" role="alert">
+        </div>
+    </div>
+    <script>
+      
+    </script>
+<?php include 'footer.php' ?>
