e14d2d9ba6e4312c4b7f6145a48cffa9f64c1446
Lucas Overbey <lucasaoverbey@gmail.com>
2022-04-26 18:28:58 -0500

    fixed small error that allowed adminheader to print twice on orderlistadmin page

--- assets/php/orderListAdmin.php
+++ assets/php/orderListAdmin.php
@@ -1,6 +1,5 @@
 <?php include 'header.php';
       include_once ".env.php";
-      include 'adminHeader.php';
 ?>
     <div class = "container orders">
         <div class="alert alert-success" role="alert">
