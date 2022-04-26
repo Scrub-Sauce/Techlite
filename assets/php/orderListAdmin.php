8d4c8ae1f882e2dcfbe2214f4f7a31108283b0ad
Lucas Overbey <lucasaoverbey@gmail.com>
2022-04-26 18:27:36 -0500

    added temporary stuff to orderlistadmin page

--- assets/php/orderListAdmin.php
+++ assets/php/orderListAdmin.php
@@ -11,10 +11,10 @@
                 <p>Customer name</p>
             </div>
             <div class = "col-2">
-                    <p>Order Number</p>
+                <p>Order Number</p>
             </div>
             <div class = "col-2">
-                    <p>Order total</p>
+                <p>Order total</p>
             </div>
             <div class = "col-2">
                 <p>Total items</p>
@@ -27,7 +27,51 @@
             </div>
         </div>
         <div class = "orderList">
+        <div id="accordion">
+            <div class="card">
+                <div class="card-header" id="headingOne">
+                <h5 class="mb-0">
+                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    #1
+                    </button>
+                </h5>
+                </div>
 
+                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                <div class="card-body">
+                    Display Data
+                </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" id="headingTwo">
+                <h5 class="mb-0">
+                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    #2
+                    </button>
+                </h5>
+                </div>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                <div class="card-body">
+                    Display Data
+                </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" id="headingThree">
+                <h5 class="mb-0">
+                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    #3
+                    </button>
+                </h5>
+                </div>
+                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                <div class="card-body">
+                    Display Data
+                </div>
+                </div>
+            </div>
+            </div>  
         </div>
     </div>
     <script>
